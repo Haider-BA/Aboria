da2dbf6cdce7d872b0b7c36db40c6557cc18611d
Martin Robinson <martinjrobins@gmail.com>
2015-02-21 02:57:47 +0000

    some random stuff in symbolic

--- src/Symbolic.h
+++ src/Symbolic.h
@@ -26,6 +26,83 @@
 #define SYMBOLIC_H_
 
 #include "Particles.h"
+
+#include <boost/proto/core.hpp>
+#include <boost/proto/context.hpp>
+namespace proto = boost::proto;
+using proto::_;
+
+template<typename Expr>
+struct particle_expression;
+
+// Tell proto how to generate expressions in the particle_domain
+struct particle_domain
+  : proto::domain<proto::generator<particle_expression> >
+{};
+
+// Will be used to define the placeholders
+template<int I> struct placeholder {};
+
+// Define a particle context, for evaluating arithmetic expressions
+struct particle_context
+  : proto::callable_context< particle_context const >
+{
+    // The values bound to the placeholders
+    double d[2];
+    ParticleType& p;
+
+    // The result of evaluating arithmetic expressions
+    typedef double result_type;
+
+    explicit particle_context(ParticleType &p): p(p) {}
+
+    // Handle the evaluation of the placeholder terminals
+    template<int I>
+    ParticleType::get_data_elem_type<I> operator ()(proto::tag::terminal, placeholder<I>) const
+    {
+        return p.get_data_elem<I>();
+    }
+};
+
+// Wrap all particle expressions in this type, which defines
+// operator () to evaluate the expression.
+template<typename Expr>
+struct particle_expression
+  : proto::extends<Expr, particle_expression<Expr>, particle_domain>
+{
+    explicit particle_expression(Expr const &expr = Expr())
+      : particle_expression::proto_extends(expr)
+    {}
+
+    BOOST_PROTO_EXTENDS_USING_ASSIGN(particle_expression<Expr>)
+
+    // Override operator () to evaluate the expression
+    double operator ()() const
+    {
+        particle_context const ctx;
+        return proto::eval(*this, ctx);
+    }
+
+    double operator ()(double d1) const
+    {
+        particle_context const ctx(d1);
+        return proto::eval(*this, ctx);
+    }
+
+    double operator ()(double d1, double d2) const
+    {
+        particle_context const ctx(d1, d2);
+        return proto::eval(*this, ctx);
+    }
+};
+
+// Define some placeholders (notice they're wrapped in particle_expression<>)
+particle_expression<proto::terminal< placeholder< 1 > >::type> const _1;
+particle_expression<proto::terminal< placeholder< 2 > >::type> const _2;
+
+
+
+
 #include <boost/proto/proto.hpp>
 namespace proto = boost::proto;
 
